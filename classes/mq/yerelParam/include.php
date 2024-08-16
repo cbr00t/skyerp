be421b1911531e47893fca67c4a86ccd2e53899b
Özer <83964758+cbr00t@users.noreply.github.com>
2024-08-16 18:18:07 +0300

    Update include.php

--- classes/mq/yerelParam/include.php
+++ classes/mq/yerelParam/include.php
@@ -1,5 +1,3 @@
 <?php $_partRoot = "$webRoot/classes/mq/yerelParam" ?>
-<script src="<?=$_partRoot?>/mqYerelParam-base.js?<?=$appVersion?>"></script>
-<script src="<?=$_partRoot?>/mqYerelParam-ortak.js?<?=$appVersion?>"></script>
-<script src="<?=$_partRoot?>/mqYerelParam-app.js?<?=$appVersion?>"></script>
-<script src="<?=$_partRoot?>/mqYerelParam-config.js?<?=$appVersion?>"></script>
+<script src="<?=$_partRoot?>/mqYerelParam-base.js?<?=$appVersion?>"></script> <script src="<?=$_partRoot?>/mqYerelParam-ortak.js?<?=$appVersion?>"></script>
+<script src="<?=$_partRoot?>/mqYerelParam-app.js?<?=$appVersion?>"></script> <script src="<?=$_partRoot?>/mqYerelParam-config.js?<?=$appVersion?>"></script>
