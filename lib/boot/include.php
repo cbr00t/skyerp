6be3af8fb8211fe4ca21a1768477ba7407c0a291
Özer <83964758+cbr00t@users.noreply.github.com>
2024-09-11 14:48:21 +0300

    Prefetch

--- lib/boot/include.php
+++ lib/boot/include.php
@@ -1,4 +1,4 @@
-<?php $_partRoot = "$webRoot/lib/boot"; include_once("$_partRoot/layout.php") ?>
+<?php $_partRoot = "$webRoot/lib/boot" ?>
 <link async="false" rel="stylesheet" href="<?=$_partRoot?>/boot.css?<?=$appVersion?>"/> <script async="false" src="<?=$_partRoot?>/boot.js?<?=$appVersion?>"></script>
 <link async="false" rel="stylesheet" href="./boot.css?<?=$appVersion?>"/> <script async="false" src="./boot.js?<?=$appVersion?>"></script>
-
+<?php include_once("$_partRoot/layout.php") ?>
--- lib/include/head.php
+++ lib/include/head.php
@@ -4,4 +4,6 @@
 <title><?=$appName?></title>
 <meta name="description" content="<?=$appName?>" />
 <meta name="viewport" content="width=device-width, minimum-scale=.7, maximum-scale=.7, user-scalable=yes, shrink-to-fit=yes" />
+<link rel="preload" fetchpriority="high" as="image" type="image/svg+xml" src="<?=$webRoot?>/images/sky_logo.svg"/>
+<link rel="preload" fetchpriority="high" as="image" type="image/png" src="<?=$webRoot?>/images/header-background.png"/>
 <?php require_once("$webRoot/lib/include/include-libExternal.php") ?> <?php require_once("$webRoot/lib/include/include-libExternal-jqx.php") ?>
