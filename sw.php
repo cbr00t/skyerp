d740cea185a71b1d79af9a812b2944a3eaf8603f
Özer <83964758+cbr00t@users.noreply.github.com>
2024-09-16 00:41:47 +0300

    eventStream api ve ServiceWorker

--- sw.php
+++ sw.php
@@ -1,5 +1,6 @@
 <?php header('Content-Type: text/javascript'); require_once('config.php') ?>
-const APP_NAME = '<?=$globalAppName?>', VERSION = '<?=$appVersion?>', CACHE_NAME = `cache-${APP_NAME}-${VERSION}`;
+const APP_NAME = '<?=$globalAppName?>', VERSION = '<?=$appVersion?>';
+const CACHE_NAME = `cache-${APP_NAME}-${VERSION}`, StreamHeaders = { 'text/event-stream': true, 'application/x-ndjson' };
 addEventListener('install', async e => {
     skipWaiting(); const staticAssets = ['./', './lib', './ortak', './app', './images'];
     const cache = await caches.open(CACHE_NAME); for (const url of staticAssets) { try { cache.add(url) } catch (ex) { } }
@@ -23,7 +24,7 @@ async function handleFetchFromNetwork(req) {
 	const cache = await caches.open(CACHE_NAME);
 	try { 
 		const resp = await fetch(req);
-		if (req.method == 'GET') { try { await cache.put(req, await resp.clone()) } catch (ex) { } }
+		if (req.method == 'GET' && !StreamHeaders[req.headers?.get('Content-Type')]) { try { await cache.put(req, await resp.clone()) } catch (ex) { } }
 		return resp
 	} catch (ex) { const cachedResponse = await cache.match(req); if (!cachedResponse) { throw ex } return cachedResponse }
 }
