f2fb30e0f453e11816b18f0cc9178799ec49c727
Özer <83964758+cbr00t@users.noreply.github.com>
2024-10-14 18:55:05 +0300

    include yapısı düzeltmeler

--- classes/mq/include.php
+++ classes/mq/include.php
@@ -1,5 +1,4 @@
 <?php
-	$_partRoot = "$webRoot/classes/mq";
-	require_once("$_partRoot/include-01.php"); require_once("$_partRoot/include-02.php");
+	$_partRoot = "$webRoot/classes/mq"; require_once("$_partRoot/include-01.php"); require_once("$_partRoot/include-02.php");
 	require_once("$_partRoot/include-03.php"); require_once("$_partRoot/include-04.php")
 ?>
--- classes/mq/yerelParam/ui/yerelParam-config/include.php
+++ classes/mq/yerelParam/ui/yerelParam-config/include.php
@@ -1,4 +1,3 @@
 <?php require_once("$webRoot/classes/mq/yerelParam/ui/yerelParam-base/include.php") ?>
 <?php $_partRoot = "$webRoot/classes/mq/yerelParam/ui/yerelParam-config" ?>
-<link rel="stylesheet" href="<?=$_partRoot?>/part.css"/>
-<script src="<?=$_partRoot?>/part.js"></script>
+<link rel="stylesheet" href="<?=$_partRoot?>/part.css?<?=$appVersion?>"/> <script src="<?=$_partRoot?>/part.js?<?=$appVersion?>"></script>
