9eed5e01695c593737dedc99e82aeac54f092c88
Özer <83964758+cbr00t@users.noreply.github.com>
2025-03-19 23:21:11 +0300

    sablonluSiparisApp2 -> sablonluSiparisApp

---
+++ app/sablonluSiparis/config.php
@@ -0,0 +1,4 @@
+<?php
+	$appClass = 'SablonluSiparisApp'; $appName = 'Şablonlu Sipariş';
+	$startURL_postfix = '/app/sablonluSiparis'; $webRoot = '../..'; require_once("$webRoot/config.php")
+?>
--- app/sablonluSiparis2/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$appClass = 'SablonluSiparisApp'; $appName = 'Şablonlu Sipariş';
-	$startURL_postfix = '/app/sablonluSiparis2'; $webRoot = '../..'; require_once("$webRoot/config.php")
-?>
