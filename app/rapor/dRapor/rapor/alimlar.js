06ec1ad146a5dc05b187b525299f579922b56a5d
Özer <83964758+cbr00t@users.noreply.github.com>
2025-11-25 15:06:58 +0300

    Hizmet Satış ve Alımlar

--- app/rapor/dRapor/rapor/alimlar.js
+++ app/rapor/dRapor/rapor/alimlar.js
@@ -2,7 +2,7 @@ class DRapor_StokAlimlar extends DRapor_TicariStok {
 	static { window[this.name] = this; this._key2Class[this.name] = this }
 	static get kategoriKod() { return 'ALIM' } static get kategoriAdi() { return 'Alımlar' }
 	static get altRaporClassPrefix() { return 'DRapor_StokAlimlar' } static get vioAdim() { return 'AL-RCF' }
-	static get kod() { return 'STALIM' } static get aciklama() { return 'Alımlar' }
+	static get kod() { return 'STALIM' } static get aciklama() { return 'Alımlar (<span class=royalblue>Stok</span>)' }
 }
 class DRapor_StokAlimlar_Main extends DRapor_Sevkiyat_Main {
 	static { window[this.name] = this; this._key2Class[this.name] = this }
@@ -12,7 +12,20 @@ class DRapor_StokAlimlar_Main extends DRapor_Sevkiyat_Main {
 		wh.add(`fis.almsat IN ('A', 'M')`)
 	}
 }
-class DRapor_HizmetAlimlar_Main extends DRapor_StokAlimlar_Main {
+
+class DRapor_HizmetAlimlar extends DRapor_TicariHizmet {
+	static { window[this.name] = this; this._key2Class[this.name] = this }
+	static get kategoriKod() { return DRapor_StokAlimlar.kategoriKod }
+	static get kategoriAdi() { return DRapor_StokAlimlar.kategoriAdi }
+	static get altRaporClassPrefix() { return 'DRapor_HizmetAlimlar' }
+	static get vioAdim() { return DRapor_StokAlimlar_Main.vioAdim }
+	static get kod() { return 'HZALIM' } static get aciklama() { return 'Alımlar (<span class=orangered>Hizmet</span>)' }
+}
+class DRapor_HizmetAlimlar_Main extends DRapor_Sevkiyat_Main {
 	static { window[this.name] = this; this._key2Class[this.name] = this }
 	static get raporClass() { return DRapor_HizmetAlimlar }
+	fisVeHareketBagla({ sent: { where: wh }}) {
+		super.fisVeHareketBagla(...arguments)
+		wh.add(`fis.almsat IN ('A', 'M')`)
+	}
 }
--- app/rapor/dRapor/rapor/satislar.js
+++ app/rapor/dRapor/rapor/satislar.js
@@ -2,7 +2,7 @@ class DRapor_StokSatislar extends DRapor_TicariStok {
 	static { window[this.name] = this; this._key2Class[this.name] = this }
 	static get kategoriKod() { return 'SATIS' } static get kategoriAdi() { return 'Satışlar' }
 	static get altRaporClassPrefix() { return 'DRapor_StokSatislar' } static get vioAdim() { return 'SA-RCF' }
-	static get kod() { return 'STSATIS' } static get aciklama() { return 'Satışlar' }
+	static get kod() { return 'STSATIS' } static get aciklama() { return 'Satışlar (<span class=royalblue>Stok</span>)' }
 }
 class DRapor_StokSatislar_Main extends DRapor_Sevkiyat_Main {
 	static { window[this.name] = this; this._key2Class[this.name] = this }
@@ -12,7 +12,20 @@ class DRapor_StokSatislar_Main extends DRapor_Sevkiyat_Main {
 		wh.add(`fis.almsat = 'T'`)
 	}
 }
-class DRapor_HizmetSatislar_Main extends DRapor_StokSatislar_Main {
+
+class DRapor_HizmetSatislar extends DRapor_TicariHizmet {
+	static { window[this.name] = this; this._key2Class[this.name] = this }
+	static get kategoriKod() { return DRapor_StokSatislar.kategoriKod }
+	static get kategoriAdi() { return DRapor_StokSatislar.kategoriAdi }
+	static get altRaporClassPrefix() { return 'DRapor_HizmetSatislar' }
+	static get vioAdim() { return DRapor_StokSatislar_Main.vioAdim }
+	static get kod() { return 'HZSATIS' } static get aciklama() { return 'Satışlar (<span class=orangered>Hizmet</span>)' }
+}
+class DRapor_HizmetSatislar_Main extends DRapor_Sevkiyat_Main {
 	static { window[this.name] = this; this._key2Class[this.name] = this }
 	static get raporClass() { return DRapor_HizmetSatislar }
+	fisVeHareketBagla({ sent: { where: wh }}) {
+		super.fisVeHareketBagla(...arguments)
+		wh.add(`fis.almsat = 'T'`)
+	}
 }
